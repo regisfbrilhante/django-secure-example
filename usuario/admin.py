6f65c183307d4782b1c073b02d9dfe6861ae929d
Régis Brilhante <regisfbrilhante@gmail.com>
2019-06-07 19:12:20 -0300

    renaming model author to usuario

--- usuario/admin.py
+++ usuario/admin.py
@@ -2,12 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Author
+from .models import Usuario
 
 
-class AuthorAdmin(admin.ModelAdmin):
-    date_hierarchy = 'pub_date'
-    pass
 
-
-admin.site.register(Author, AuthorAdmin)
+admin.site.register(Usuario)
--- usuario/models.py
+++ usuario/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib import admin
+
 # Create your models here.
 
 
-class Author(models.Model):
+class Usuario(models.Model):
     name = models.CharField(max_length=10)
     pub_date = models.DateTimeField(auto_now_add=True)
 
